8592aec1188248188bcdc8a1de97f903793c5bd5
trienvu135 <trienvu135@1de66c78-1236-ea1c-c22f-251e597c1ccc>
2014-09-26 18:12:35 +0000

    xxx

---
+++ trunk/HayChonGiaDung/src/com/gtotech/app/customview/ButtonCustomView.java
@@ -0,0 +1,42 @@
+package com.gtotech.app.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+import com.gtoteck.app.haychongiadung.R;
+
+public class ButtonCustomView extends Button {
+
+	public ButtonCustomView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init(attrs);
+	}
+
+	public ButtonCustomView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs);
+
+	}
+
+	public ButtonCustomView(Context context) {
+		super(context);
+		init(null);
+	}
+
+	private void init(AttributeSet attrs) {
+		if (attrs != null) {
+			TypedArray a = getContext().obtainStyledAttributes(attrs,
+					R.styleable.MyTextView);
+			String fontName = a.getString(R.styleable.MyTextView_fontName);
+			if (fontName != null) {
+				Typeface myTypeface = Typeface.createFromAsset(getContext()
+						.getAssets(), "fonts/" + fontName);
+				setTypeface(myTypeface);
+			}
+			a.recycle();
+		}
+	}
+
+}
